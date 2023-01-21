f5ea8d229c0aecb1300ed2a1f3286b15b2430603
zeroThousand111 <zeroThousand111@proton.me>
2023-01-21 21:38:48 +0000

    add movies_array.rb file for exercise 3 in The Basics

---
+++ movies_array.rb
@@ -0,0 +1,15 @@
+movies_list = {
+  :alien => 1978,
+  :jaws => 1975,
+  :star_wars => 1977,
+  :snakes_on_a_plane => 2006
+}
+puts movies_list[:alien]
+puts movies_list[:jaws]
+puts movies_list[:star_wars]
+puts movies_list[:snakes_on_a_plane]
+
+=begin
+# alternatively I could have done the above with one line of code:
+puts movies_list.values
+=end
