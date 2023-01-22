649fe09e51c451d3be83f58fed96e1bde8f58338
zeroThousand111 <zeroThousand111@proton.me>
2023-01-22 08:06:01 +0000

    create factorial.rb to solve exercise 5

---
+++ the_basics/factorial.rb
@@ -0,0 +1,9 @@
+puts "The factorials of 5, 6, 7 & 8 are..."
+five = 5 * 4 * 3 * 2 * 1
+six = 6 * 5 * 4 * 3 * 2 * 1
+seven = 7 * 6 * 5 * 4 * 3 * 2 * 1
+eight = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts "The factorial of 5 is #{five}!"
+puts "The factorial of 6 is #{six}!"
+puts "The factorial of 7 is #{seven}!"
+puts "The factorial of 8 is #{eight}!"
