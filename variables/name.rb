cccaa0c757b96ada5e6a3262963759247a832ffb
zeroThousand111 <zeroThousand111@proton.me>
2023-01-22 19:34:50 +0000

    amend name.rb file to input first name and last name and concatenate them for an output

--- variables/name.rb
+++ variables/name.rb
@@ -1,5 +1,8 @@
-puts "Please enter your name:"
-user_name = gets.chomp
+puts "Please enter your FIRST name:"
+first_name = gets.chomp
+puts "Please enter your LAST name:"
+last_name = gets.chomp
+user_name = first_name + " " + last_name
 puts "Hello there, #{user_name}!" 
 puts " - - - "
 10.times {puts "#{user_name}!"}
