b851da8bc85298455bea4db170e029379db10b98
zeroThousand111 <zeroThousand111@proton.me>
2023-01-22 19:20:52 +0000

    create age.rb to answer exercise 2

---
+++ variables/age.rb
@@ -0,0 +1,16 @@
+# input
+puts "How old are you?"
+age = gets.to_i
+
+# processing
+age_10 = age + 10
+age_20 = age + 20
+age_30 = age + 30
+age_40 = age + 40
+
+# output
+puts "You are #{age} years old."
+puts "In 10 years time, you will be #{age_10} years old."
+puts "In 20 years time, you will be #{age_20} years old."
+puts "In 30 years time, you will be #{age_30} years old."
+puts "In 40 years time, you will be #{age_40} years old."
