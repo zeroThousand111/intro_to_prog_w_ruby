9c1a8e3d0a15d9fd901b8910a617f9fbaefd68f5
zeroThousand111 <zeroThousand111@proton.me>
2023-01-29 20:37:27 +0000

    updated practice_each.rb with alternate do/end syntax for looping block

--- loops/practice_each.rb
+++ loops/practice_each.rb
@@ -1,5 +1,20 @@
 # practice_each.rb
 
+=begin
+This is the first version of the code that I've coded out to not interfere with the updated code added later
+
 names = ['Bob', 'Joe', 'Steve', 'Janice', 'Susan', 'Helen']
 
 names.each { |name| puts name }
+=end
+
+# Below is the second version using do/end instead of {}
+
+
+names = ['Bob', 'Joe', 'Steve', 'Janice', 'Susan', 'Helen']
+x = 1
+
+names.each do |name|
+  puts "#{x}. #{name}"
+  x += 1
+end
