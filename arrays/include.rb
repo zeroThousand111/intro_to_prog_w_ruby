a9212f17599d8b0511181e294bcff7133fedca66
zeroThousand111 <zeroThousand111@proton.me>
2023-01-31 20:34:52 +0000

    allow searched for number to be a variable instead of 3

--- arrays/include.rb
+++ arrays/include.rb
@@ -1,11 +1,11 @@
 arr = [1, 3, 5, 7, 9, 11]
 
-# number = 3
+number = 3
 
-test = arr.include?(3)
+test = arr.include?(number)
 
 if test == true
-  puts "There is a 3 in the array!"
+  puts "There is a #{number} in the array!"
 else 
-  puts "There is not any 3 in the array!"
+  puts "There is not any #{number} in the array!"
 end
