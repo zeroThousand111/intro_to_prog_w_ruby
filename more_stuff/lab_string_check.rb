52d99f6e2664d7cc9c2d43c70d14c5c522a63f3a
zeroThousand111 <zeroThousand111@proton.me>
2023-02-05 14:40:00 +0000

    create lab_string_check.rb to answer exercise 1 in more stuff chapter

---
+++ more_stuff/lab_string_check.rb
@@ -0,0 +1,21 @@
+# define an array of the words to search through
+sample_words = ["laboratory", "experiment", "Pans Labyrinth", "elaborate", "polar bear"]
+
+# define a method to check for lab using regex and a conditional statement
+def check_word(word)
+   if /lab/.match(word)
+     puts "We have a match in the word '#{word}'!"
+   else
+      puts "No match here in the word '#{word}'!"
+   end
+end
+
+# call the method on the sample words in the array individually
+check_word("laboratory")
+check_word("experiment")
+check_word("Pans Labyrinth")
+check_word("elaborate")
+check_word("polar bear")
+
+# call the method on the array of words
+sample_words.each {|word| puts "#{word}"}
