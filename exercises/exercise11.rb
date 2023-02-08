81356aa40c202c91e7649f5c3684a5b62fe42d70
zeroThousand111 <zeroThousand111@proton.me>
2023-02-08 08:48:56 +0000

    create exercise11.rb to answer frustratingly-hard exercise 11

--- exercises/exercise11.rb
+++ exercises/exercise11.rb
@@ -4,13 +4,17 @@ contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
 
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-# replace blank values in contacts hash with arrays from contact_data
-contacts[:"Joe Smith"] = contact_data[0]
-contacts[:"Sally Johnstone"] = contact_data[1]
+# Now replace blank values in contacts hash with with input from nested arrays in contact_data
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
+
 
 # output key references from changed contacts hash one at a time
-p contacts[:"Joe Smith"]
-p contacts[:"Sally Johnstone"]
+p contacts
 
 # Expected output:
 #  {
