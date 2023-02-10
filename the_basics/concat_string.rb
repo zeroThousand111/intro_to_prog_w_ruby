e0477a2cf8592eca801401775c7e49b8beafe181
zeroThousand111 <zeroThousand111@proton.me>
2023-02-10 17:18:43 +0000

    create concat_string.rb to answer exercise 1

---
+++ the_basics/concat_string.rb
@@ -0,0 +1,12 @@
+# get user input
+puts "Enter firstname:"
+first_name = gets.chomp
+puts "Enter last name:"
+last_name = gets.chomp
+
+
+# concatenate strings
+full_name = first_name + " " + last_name
+
+# output full name
+puts "Your name is... #{full_name}!"
