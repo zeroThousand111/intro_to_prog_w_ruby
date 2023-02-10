75ee54d7b8112181b5d741c742bb4a8178297d88
zeroThousand111 <zeroThousand111@proton.me>
2023-02-10 17:36:11 +0000

    create find_the_digit2.rb to answer exercise 2 for a second time

---
+++ the_basics/find_the_digit2.rb
@@ -0,0 +1,18 @@
+# get user input
+puts "Please enter a four-digit number:"
+number = gets.chomp.to_i
+
+
+# process input number
+thousand_place_value = number / 1000
+hundred_place_value = number % 10 /100
+ten_place_value = 
+single_place_value = number % 10
+
+# output
+puts "Your number was #{number}."
+puts "That means..."
+puts "The thousand place value is #{thousand_place_value}."
+puts "The hundred place value is #{hundred_place_value}."
+puts "The tens place value is #{ten_place_value}."
+puts "The single place value is #{single_place_value}."
