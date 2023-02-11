8cf5cdf5bbc6967d183cfce3ba335c0f391f6bca
zeroThousand111 <zeroThousand111@proton.me>
2023-02-11 12:50:09 +0000

    create factorial2.rb to answer exercise 5

---
+++ the_basics/factorial2.rb
@@ -0,0 +1,12 @@
+# calculate factorials
+five_factorial = 5 * 4 * 3 * 2 * 1
+six_factorial = 6 * 5 * 4 * 3 * 2 * 1
+seven_factorial = 7 * 6 * 5 * 4 * 3 * 2 * 1
+eight_factorial = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# output factorials
+puts five_factorial
+puts six_factorial
+puts seven_factorial
+puts eight_factorial
+
