725aedf939bec7f8836c632daca6540934a46aa6
zeroThousand111 <zeroThousand111@proton.me>
2023-02-11 09:00:41 +0000

    create movie_hash to answer exercise 3

---
+++ the_basics/movie_hash.rb
@@ -0,0 +1,7 @@
+movies = {King_Kong: 1911, Donkey_Kong: 2011, Jaws: 1975, Star_Wars: 1977, Some_old_trash: 2020}
+
+puts movies[:King_Kong]
+puts movies[:Donkey_Kong]
+puts movies[:Jaws]
+puts movies[:Star_Wars]
+puts movies[:Some_old_trash]
