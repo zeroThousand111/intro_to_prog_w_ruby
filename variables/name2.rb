f3995150346f0c4dbd7556fbabb76fb4cd80e8b4
zeroThousand111 <zeroThousand111@proton.me>
2023-02-12 14:47:07 +0000

    add first_name last_name user input to script

--- variables/name2.rb
+++ variables/name2.rb
@@ -1,10 +1,14 @@
-# get user name
-puts "Please enter your name:"
-name = gets.chomp
+# get user names
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "Please enter your last name:"
+last_name = gets.chomp
+
+full_name = first_name + " " + last_name
 
 # output user name with string interpolation
-puts "Hello there, #{name}!"
+puts "Hello there, #{full_name}!"
 
 # output user name ten times using times method
 
-10.times {puts name}
+10.times {puts full_name}
