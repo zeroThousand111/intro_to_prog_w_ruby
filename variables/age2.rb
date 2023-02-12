c111f9c6b73388d9a60f8a94f035d57ed09cf9d1
zeroThousand111 <zeroThousand111@proton.me>
2023-02-12 14:29:47 +0000

    create name2.rb to answer exercise 2

---
+++ variables/age2.rb
@@ -0,0 +1,11 @@
+# get user information
+puts "How old are you?"
+age = gets.chomp.to_i
+
+
+
+# process age and output future ages
+puts "In ten years time you will be #{age + 10} years old!"
+puts "In twenty years time you will be #{age + 20} years old!"
+puts "In thirty years time you will be #{age + 30} years old!"
+puts "In forty years time you will be #{age + 40} years old!"
