642170b215fd9c170bf1b33d546aee132ad9a941
zeroThousand111 <zeroThousand111@proton.me>
2023-02-13 11:42:09 +0000

    create case_practice1.rb to try out a case statement

---
+++ flow_control/case_practice1.rb
@@ -0,0 +1,10 @@
+a = 9
+
+case 
+when a >= 10
+  puts "The number is in double digits!"
+when a <10
+  puts "The number is in single digits!"
+else
+  puts "I have no idea what this number is!"
+end
