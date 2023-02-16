eea427c6c71cfa75755e7893ad34b9b3ae79234e
zeroThousand111 <zeroThousand111@proton.me>
2023-02-16 21:11:17 +0000

    create exercise4a.rb to answer exercise 4 for a second time

---
+++ exercises/exercise4a.rb
@@ -0,0 +1,11 @@
+# initialise array
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# add 11 on to array using the push method
+array.push(11)
+
+# add 0  to the beginning of the array using the unshift method
+array.unshift(0)
+
+# output new array
+p array
