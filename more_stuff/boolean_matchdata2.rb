ff26aeb53f810e3e71f927acf4a8444d40bd60d8
zeroThousand111 <zeroThousand111@proton.me>
2023-02-16 12:01:31 +0000

    create boolean_matchdata2.rb to illustrate regex search using match? method

---
+++ more_stuff/boolean_matchdata2.rb
@@ -0,0 +1,17 @@
+# get user input
+puts "Please enter a word:"
+word = gets.chomp
+puts "Please enter a letter to search for:"
+letter = gets.chomp
+
+# define search method
+def search(word, letter)
+  if word.match?(/#{letter}/)
+    puts "I've found your letter #{letter} in the word #{word}!"
+  else
+    puts "Sorry, I cannot find your letter #{letter} in the word #{word}."
+  end
+end
+
+# invoke search method on user input
+search(word, letter)
