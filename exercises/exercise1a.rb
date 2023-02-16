acf039db3e5f68bef52b7dedb58ace4bfd6a0faa
zeroThousand111 <zeroThousand111@proton.me>
2023-02-16 20:47:18 +0000

    create exercise1a.rb to answer exercise 1

---
+++ exercises/exercise1a.rb
@@ -0,0 +1,10 @@
+# initialise array
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# iterate over array to print each value - one line version
+array.each {|x| puts x}
+
+# iterate over array to print each value - multi-line version
+array.each do |x|
+  puts x
+end
