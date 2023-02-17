3ac4193fe208555e71bf092fe76316d262b19b62
zeroThousand111 <zeroThousand111@proton.me>
2023-02-17 08:18:57 +0000

    create exercise5a.rb to answer exercise 5 a second time

---
+++ exercises/exercise5a.rb
@@ -0,0 +1,12 @@
+# initialise array
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+# remove the 11 from the array using the pop method
+array.pop
+
+# add another 3 to the array using the shovel method
+array << 3
+
+# output new array
+p array
+puts array
