cee039008f6b231d2e3841263bfdd9e1154ae866
zeroThousand111 <zeroThousand111@proton.me>
2023-02-17 16:35:02 +0000

    create challenge16a.rb to answer challenge 16 a second time

---
+++ exercises/challenge16a.rb
@@ -0,0 +1,9 @@
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+fields = [:email, :address, :phone]
+
+# 
+
+contacts.each {|contact, hash| fields.each {|field| hash[field] = contact_data.shift}}
+
+p contacts
