f3870ca534e223744db7938fc1da182467650460
Graham Jarvis <zeroThousand111@proton.me>
2023-04-05 12:32:18 +0000

    create lab_string_check3.rb to answer exercise 1 in More Stuff section

---
+++ more_stuff/lab_string_check3.rb
@@ -0,0 +1,9 @@
+def lab_check(string)
+    puts string if string =~ /lab/
+end
+
+lab_check("laboratory")
+lab_check("experiment")
+lab_check("Pans Labyrinth")
+lab_check("elaborate")
+lab_check("polar bear")
